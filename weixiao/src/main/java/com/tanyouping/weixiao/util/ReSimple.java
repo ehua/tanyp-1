9d277e2e32e0aa9617666761c2f3a65d5992fd7f
tanyp <652630339@qq.com>
2018-02-10 09:47:22 +0800

    default

---
+++ weixiao/src/main/java/com/tanyouping/weixiao/util/ReSimple.java
@@ -0,0 +1,42 @@
+package com.tanyouping.weixiao.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by tanyp on 2018/2/8
+ * StackOverflowError and OutOfMemoryError
+ *
+ */
+public class ReSimple {
+
+    private int stackSize = 0;
+
+    public void add(){
+        stackSize++;
+        add();
+    }
+
+    public static void main(String[] args) {
+
+        ReSimple test = new ReSimple();
+        try {
+            test.add();
+        }catch (StackOverflowError e){
+            System.out.println(test.stackSize);
+            throw e;
+        }
+
+
+        List<ReSimple> strings = new ArrayList<>();
+        try {
+            while (true)
+                strings.add(new ReSimple());
+        }catch (OutOfMemoryError e){
+            System.out.println(strings.size());
+            throw  e;
+        }
+
+    }
+
+}
