506ea65e94ff0bdf3cbd31df409ec5ce5f0fb99f
tanyp <652630339@qq.com>
2018-08-13 09:42:50 +0800

    ssm

--- tanyp/src/main/java/me/tanyp/interceptor/GlobalHandleInterceptor.java
+++ tanyp/src/main/java/me/tanyp/interceptor/GlobalHandleInterceptor.java
@@ -1,9 +1,43 @@
 package me.tanyp.interceptor;
 
+import me.tanyp.util.StringUtils;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Created by tanyp on 2018/8/12
  */
 public class GlobalHandleInterceptor implements HandlerInterceptor {
+
+    public GlobalHandleInterceptor() {
+    }
+
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        return true;
+    }
+
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+    }
+
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+    }
+
+    private String getRealIp(HttpServletRequest request) {
+        String ip = request.getHeader("x-forwarded-for");
+        if (StringUtils.isEmpty(ip) || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+
+        if (StringUtils.isEmpty(ip) || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+
+        if (StringUtils.isEmpty(ip) || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        return ip;
+    }
 }
